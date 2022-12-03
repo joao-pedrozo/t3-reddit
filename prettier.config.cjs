100b4b573e1b20db79d369f743b276d3086b492d
Joao Pedrozo <jp.pedrozo@hotmail.com>
2022-12-03 18:18:04 -0300

    style: run prettier to style files

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,4 +1,9 @@
 /** @type {import("prettier").Config} */
 module.exports = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  plugins: [require.resolve('prettier-plugin-tailwindcss')],
+  semi: true,
+  trailingComma: 'es5',
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
 };
